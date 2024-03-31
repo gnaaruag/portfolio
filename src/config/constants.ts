d7e7870b6671dc4c690bd0d521c15a1708b4c638
Gaurang Ratnaparkhi <68043860+gnaaruag@users.noreply.github.com>
2024-03-31 21:03:10 +0530

    fixed access token issue

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const access_token = "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";
-export const mail_service = "service_msdz39m";
-export const mail_template = "template_8lmpx77";
-export const mail_token = "4QayQfv8sbEhkuu4s";
+export const access_token = import.meta.env.VITE_GH_TOKEN;
+export const mail_service = import.meta.env.VITE_MAIL_SERVICE;
+export const mail_template = import.meta.env.VITE_MAIL_TEMPLATE;
+export const mail_token = import.meta.env.VITE_MAIL_TOKEN;
